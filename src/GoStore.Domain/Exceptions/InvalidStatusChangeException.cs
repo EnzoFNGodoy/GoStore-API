685d0ac87d769753be80b6102d1883b7d3e12975
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-01-03 02:46:59 -0300

    adding the sonarqube tags to be ignored in .gitignore

--- src/GoStore.Domain/Exceptions/InvalidStatusChangeException.cs
+++ src/GoStore.Domain/Exceptions/InvalidStatusChangeException.cs
@@ -1,7 +1,14 @@
-﻿namespace GoStore.Domain.Exceptions;
+﻿using System.Runtime.Serialization;
 
+namespace GoStore.Domain.Exceptions;
+
+[Serializable]
 public sealed class InvalidStatusChangeException : Exception
 {
+    private InvalidStatusChangeException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    { }
+
     public InvalidStatusChangeException(string message = "The status cannot be changed. The order is invalid.")
         : base(message) { }
 
