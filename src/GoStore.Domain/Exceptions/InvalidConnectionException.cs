7a26b27abe7bce0fb0071d3365699d1596b87719
EnzoFNGodoy <enzofngodoy@hotmail.com>
2023-01-04 03:33:52 -0300

    Adding MongoDbContext

---
+++ src/GoStore.Domain/Exceptions/InvalidConnectionException.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace GoStore.Domain.Exceptions;
+
+[Serializable]
+public sealed class InvalidConnectionException : Exception
+{
+    private InvalidConnectionException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    { }
+
+    public InvalidConnectionException(string message = "The connection is refused or incorrect. Please retry with new credentials.")
+        : base(message) { }
+
+    public InvalidConnectionException(string message, Exception inner)
+        : base(message, inner) { }
+}
---
+++ src/GoStore.Infra.Data/Context/GoStoreContext.cs
@@ -0,0 +1,47 @@
+﻿using GoStore.Domain.Entities;
+using GoStore.Domain.Exceptions;
+using MongoDB.Driver;
+
+namespace GoStore.Infra.Data.Context;
+
+public class GoStoreContext
+{
+    public static string? ConnectionString { get; set; }
+    public static string? DatabaseName { get; set; }
+    public static bool IsSSL { get; set; }
+
+    private IMongoDatabase? _database { get; }
+
+    public GoStoreContext()
+    {
+        MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+        try
+        {
+            if (IsSSL)
+            {
+                settings.SslSettings = new SslSettings
+                { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+                var mongoClient = new MongoClient(settings);
+                _database = mongoClient.GetDatabase(DatabaseName);
+            }
+        }
+        catch (Exception)
+        {
+            throw new InvalidConnectionException();
+        }
+    }
+
+    #region Collections
+    public IMongoCollection<Customer> Customers
+        => _database.GetCollection<Customer>("Customers");
+
+    public IMongoCollection<Product> Products
+        => _database.GetCollection<Product>("Products");
+
+    public IMongoCollection<CreditCardPayment> CreditCardPayments
+        => _database.GetCollection<CreditCardPayment>("CreditCardPayments");
+
+    public IMongoCollection<Order> Orders
+      => _database.GetCollection<Order>("Orders");
+    #endregion
+}
